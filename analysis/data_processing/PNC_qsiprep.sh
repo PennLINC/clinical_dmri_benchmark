55c27fb4bd3a8f7eeefb7c80cdbd48d463fbcedc
ameliecr <amelie.caitlin.rauland@rwth-aachen.de>
2024-09-06 11:33:05 -0400

    Adjustments after debugging on SLURM

--- analysis/data_processing/PNC_qsiprep.sh
+++ analysis/data_processing/PNC_qsiprep.sh
@@ -9,10 +9,10 @@
 
 
 SIMG="${HOME}"/images/qsiprep-0.21.4.sif
-CODE_DIR="${HOME}"/clinical_dmri_benchmark/code
+FREESURFER_DIR="${HOME}"/software/freesurfer
 SUBJECT_LIST="${HOME}"/clinical_dmri_benchmark/analysis/data_processing/subject_lists/subject_list.txt
-DATA_ROOT="/cbica/comp_space/clinical_dmri_benchmark/data/PNC/"
-OUTPUTS="${HOME}/results/qsiprep_outputs"
+DATA_ROOT=/cbica/comp_space/clinical_dmri_benchmark/data/PNC/BIDS
+OUTPUTS="${HOME}"/results/qsiprep_outputs
 
 [ -z "${JOB_ID}" ] && JOB_ID=TEST
 
@@ -26,25 +26,32 @@ fi
 set -e -u -x
 
 # Get the subject id from the call
-subid=(head -n ${SLURM_ARRAY_TASK_ID} ${SUBJECT_LIST} | tail -n 1)
+subid=$(head -n ${SLURM_ARRAY_TASK_ID} ${SUBJECT_LIST} | tail -n 1)
 
 # Use $TMP as the workdir
-WORKDIR=${TMP}/"job-${JOB_ID}_${subid}_${sesid}"
+WORKDIR=${TMP}/"job-${JOB_ID}_${subid}"
 mkdir -p "${WORKDIR}"
 cd ${WORKDIR}
+mkdir -p BIDS
 
 # Copy the files we need from the source directory
-cp ${CODE_DIR}/license.txt ./
-cp ${CODE_DIR}/dataset_description.json BIDS/
-cp -r ${DATA_ROOT}/${subid} BIDS/${subid}
+cp ${FREESURFER_DIR}/license.txt ./
+cp ${DATA_ROOT}/dataset_description.json BIDS/
+mkdir -p BIDS/${subid}/ses-PNC1/dwi
+mkdir -p BIDS/${subid}/ses-PNC1/anat
+cp -r -L ${DATA_ROOT}/${subid}/ses-PNC1/dwi BIDS/${subid}/ses-PNC1/dwi
+cp -r -L ${DATA_ROOT}/${subid}/ses-PNC1/anat BIDS/${subid}/ses-PNC1/anat
 
+ls ${WORKDIR}
+ls ${WORKDIR}/BIDS
+ls ${WORKDIR}/BIDS/${subid}/ses-PNC1/dwi
 # Do the run
 qsiprep_failed=0
 singularity run \
     --containall \
     -B ${PWD} \
     ${SIMG} \
-    ${DATA_ROOT} \
+    ${PWD}/BIDS \
     ${PWD}/results \
     participant \
     -w ${PWD}/wkdir \
@@ -52,7 +59,6 @@ singularity run \
     --fs-license-file ${PWD}/license.txt \
     --skip-bids-validation \
     --participant-label "$subid" \
-    --unringing-method rpg \
     --output-resolution 1.7 \
     --ignore fieldmaps \
     --separate-all-dwis \
@@ -65,18 +71,19 @@ if [ ${qsiprep_failed} -gt 0 ]; then
     echo QSIPREPFAIL
     FAILS="${OUTPUTS}/failures/${subid}"
     mkdir -p ${FAILS}
-    fail_logs=$(find results -name 'crash*.txt')
+    fail_logs=$(find ${PWD}/results -name 'crash*.txt')
 
     # Copy the failed subject log to the bucket
-    mv $fail_logs ${FAILS}/
+    mv ${fail_logs} ${FAILS}/crash*.txt
 
     rm -rf "${WORKDIR}"
     exit 1
 fi
 
 # Move results from TMP to project directory
+mkdir -p ${OUTPUTS}/${subid}
 mv -v ${PWD}/results/qsiprep/${subid}/* ${OUTPUTS}/${subid}/
 mv ${PWD}/results/qsiprep/${subid}.html ${OUTPUTS}/${subid}.html
 mv ${PWD}/results/qsiprep/dwiqc.json ${OUTPUTS}/dwiqc.json
 
-echo SUCCESS
+echo SUCCESSC
